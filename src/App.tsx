24b2217ceb95008a4f5e3873f128e6d714b88190
Kaushik Kundu <kaushik-tech@hotmail.com>
2024-11-20 06:28:41 -0500

    Color Mode Switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import {Grid, GridItem, Show} from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
            base: `"nav" "main"`,
           lg: `"nav nav" "aside main"`
        }}>
-           <GridItem area="nav" bg="coral">Nav</GridItem>
+           <GridItem area="nav" ><NavBar/></GridItem>
            <Show above="lg" >
            <GridItem area="aside" bg="gold">Aside</GridItem>
            </Show>
