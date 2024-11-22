9f22ff23ba9741b6258de79a018df6221ef3279e
Kaushik Kundu <kaushik-tech@hotmail.com>
2024-11-21 19:29:56 -0500

    Adding Genre

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import {Grid, GridItem, Show} from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +15,7 @@ function App() {
        }}>
            <GridItem area="nav"><NavBar/></GridItem>
            <Show above="lg" >
-           <GridItem area="aside">Aside</GridItem>
+           <GridItem area="aside"><GenreList/></GridItem>
            </Show>
            <GridItem area="main"><GameGrid/></GridItem>
        </Grid>
